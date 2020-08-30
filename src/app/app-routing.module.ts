297d3d416e7f05ffa3f75a900591234ef264bbb8
wenyongqd <wenyongqd@gmail.com>
2020-08-29 23:20:53 -0400

    first submit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './view/index/index.component';
+import { ChooseComponent } from './view/choose/choose.component'
+import { NewstockComponent } from './view/newstock/newstock.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:IndexComponent
+  },
+  {
+    path:"screener",
+    component:ChooseComponent
+  },
+  {
+    path:"ngss",
+    component:NewstockComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/view/newstock/newstock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newstock',
+  templateUrl: './newstock.component.html',
+  styleUrls: ['./newstock.component.css']
+})
+export class NewstockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
